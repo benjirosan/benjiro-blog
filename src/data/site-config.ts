b6f4658387aaa9251be7493e3404db4010d88cf1
Ben Hammersley <ben@hungrysky.com>
2024-04-02 17:00:16 -0400

    Change site title and deleted placeholder posts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,8 +38,8 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: "Benjiro's Recovery Notes",
+    subtitle: 'Field notes on trauma, shame, and addiction',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
