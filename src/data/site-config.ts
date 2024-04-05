e5120a32c8935b5886ffce933826549d10a706d6
Ben Hammersley <ben@hungrysky.com>
2024-04-05 19:07:45 -0400

    Modified substack URL to remove /embed

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -116,7 +116,7 @@ const siteConfig: SiteConfig = {
     subscribe: {
         title: 'Subscribe to the Newsletter',
         text: 'Get new posts via email',
-        formUrl: 'https://benjiro.substack.com/embed'
+        formUrl: 'https://benjiro.substack.com'
     },
     postsPerPage: 8,
     projectsPerPage: 8
